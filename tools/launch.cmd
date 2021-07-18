a3a9ee44f0b0063c4414a9805b91b7e7644695a4
Anthony Ilareguy <anthony@ilareguy.com>
2021-07-18 14:15:29 -0400

    Settings update

--- tools/launch.cmd
+++ tools/launch.cmd
@@ -1,4 +1,2 @@
-install.cmd
-
 @rem Launch Arma 3 server
 start "Arma3" /min /wait arma3server_x64.exe "-mod=@CBA_A3;curator;kart;heli;mark;expansion;jets;argo;orange;tacops;tank;enoch" "-serverMod=@mafia;" "-config=@mafia\server.cfg" -port=2302 -profiles=SC "-cfg=@mafia\basic.cfg" -name=SC -autoInit
